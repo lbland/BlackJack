72914659ac7687d29fce97eeb0a619a85c91035d
Loren Bland <lorenbland@Lorens-MacBook-Pro.local>
2013-10-21 16:09:25 -0700

    Initial Commit

---
+++ BlackJack/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  BlackJack
+//
+//  Created by Loren Bland on 10/21/13.
+//  Copyright (c) 2013 Loren Bland. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
